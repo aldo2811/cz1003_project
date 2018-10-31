28a70cfbdcf55b35098f7c88f349a2157e8ea730
Aldo Halim <aldohalim28@gmail.com>
2018-10-31 23:37:58 +0800

    Major edit on map

--- project_main.py
+++ project_main.py
@@ -10,7 +10,7 @@ def load_images():
         ok_button_img: image of ok button
         ok_pressed_img: image of ok button when pressed
     """
-    map_img = pygame.image.load("map_1.png")
+    map_img = pygame.image.load("map.png")
     map_img = pygame.transform.scale(map_img, (1600, 900))          
     pin_img = pygame.image.load("pin.png")
     ok_button_img = pygame.image.load("ok_button.png")
@@ -29,7 +29,8 @@ def get_user_location(user_location, mouse):
     Returns:
         user_location: coordinates of current mouseclick with an adjustment
     """
-    screen.blit(map_img, (0, 0))
+    screen.blit(map_img, background_location)
+    screen.blit(instruction_text, instruction_location)
     screen.blit(ok_button_img, ok_location)
     # there's an offset of '-17' to adjust the pin image with the cursor
     user_location = [mouse[i] - 17 for i in range(2)]
@@ -40,32 +41,44 @@ def get_user_location(user_location, mouse):
 def display_ok_pressed():
     """Changes the ok button image to an image of a pressed ok button,
     creating a real sense of clicking the button"""
-    screen.blit(map_img, (0,0))
+    screen.blit(map_img, background_location)
+    screen.blit(instruction_text, instruction_location)
     screen.blit(pin_img, user_location)
     screen.blit(ok_pressed_img, ok_location)
 
 
 def revert_display():
     """Reverts display to the usual display with the normal ok button"""
-    screen.blit(map_img, (0,0))
+    screen.blit(map_img, background_location)
+    screen.blit(instruction_text, instruction_location)
     screen.blit(pin_img, user_location)
     screen.blit(ok_button_img, ok_location)
 
+
+def display_warning(warning):
+    if warning:
+        screen.blit(warning_text, warning_location)
+        
 
 # initialize pygame program
 pygame.init()
 resolution = (1600, 900)
 screen = pygame.display.set_mode(resolution)
 pygame.display.set_caption('NTU F/B Recommendation')
+background_location = (0, 0)
 user_location = (1600, 900)
-ok_location = (1250, 800)
-warning_location = (1050, 0)
+ok_location = (1200, 800)
+warning_location = (1150, 750)
+instruction_location = (350, 0)
 map_img, pin_img, ok_button_img, ok_pressed_img = load_images()
-screen.blit(map_img, (0,0))
+text_font = pygame.font.SysFont('calibri', 36)
+warning_text = text_font.render('Please mark your location!', False, (255, 0, 0))
+instruction_text = text_font.render('Please mark your location and click the ok button to continue.', False, (0, 0, 0))
+screen.blit(map_img, background_location)
+screen.blit(instruction_text, instruction_location)
 screen.blit(ok_button_img, ok_location)
 pygame.display.flip()
-text_font = pygame.font.SysFont('calibri', 48)
-warning_text = text_font.render('Please mark your location!', False, (255, 0, 0))
+
 clock = pygame.time.Clock()
 fps = 60
 running = True
@@ -83,35 +96,33 @@ while running:
             # detects events of left click only, so that other mouse buttons won't have any effect
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 # detects event of clicking on the map area, which drops the pin
-                if mouse[0] < 1200 or mouse[1] < 750:
+                if (mouse[0] < 1150 or mouse[1] < 750) and (65 < mouse[0] < 1535 and mouse[1] > 75):
                     pin_dropped = True
                     warning = False
                     user_location = get_user_location(user_location, mouse)
                 # detects event of clicking the ok button
-                elif 1250 <= mouse[0] <= 1555 and 800 <= mouse[1] <= 885:
+                elif 1200 <= mouse[0] <= 1505 and 800 <= mouse[1] <= 885:
                     ok_button_clicked = True
                     display_ok_pressed()
-                    if warning:
-                        screen.blit(warning_text, warning_location)
+                    display_warning(warning)
         else:
             # if location has been marked, releasing the left mouse button on the ok button area finalizes the location and ends the pygame program
             if 1250 <= mouse[0] <= 1555 and 800 <= mouse[1] <= 885:
                 if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                     if pin_dropped:
                         running = False
-                    # a warning will pop up to ensure the user picks a location before continuing
+                    # a warning will pop up to ensure the user marks their location before continuing
                     else:
                         ok_button_clicked = False
                         warning = True
                         revert_display()
-                        screen.blit(warning_text, warning_location)
+                        display_warning(warning)
             # detects event of user hovering the mouse off the ok button while holding left mouse button down
             # hence the location is not finalized
             else:
                 ok_button_clicked = False
                 revert_display()
-                if warning:
-                    screen.blit(warning_text, warning_location)
+                display_warning(warning)
 
         print(event)
 
@@ -123,9 +134,3 @@ while running:
 
 pygame.display.quit()
 pygame.quit()
-
-
-
-
-
-for i
